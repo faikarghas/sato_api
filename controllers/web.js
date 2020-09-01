05f0e41803579e3b2427338d6ef1aed296fc1a04
faikar <ghassanfaikar@yahoo.com>
2020-09-01 18:53:05 +0700

    route for web

--- controllers/web.js
+++ controllers/web.js
@@ -1,19 +1,113 @@
 const db = require('../db')
 
 module.exports = {
-    insertFaq : (req,res) => {
-        let sql = `insert into faq set ?`
+    career : (req,res) => {
+        let sql = `select * from career`
 
-        let data ={
-            question_en: req.body.question_en,
-            question_id: req.body.question_id,
-            answer_en: req.body.answer_en,
-            answer_id: req.body.answer_id
-        }
+        db.query(sql,(err,result)=>{
 
-        db.query(sql,data,(err,result)=>{
-            console.log(result);
-            res.send(result)
+            res.json({ career: result })
+        })
+    },
+    category : (req,res) => {
+        let sql = `select * from category`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ category: result })
+        })
+    },
+    faq : (req,res) => {
+        let sql = `select * from faq`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ faq: result })
+        })
+    },
+    intouch : (req,res) => {
+        let sql = `select * from intouch`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ intouch: result })
+        })
+    },
+    projectAccom : (req,res) => {
+        let sql = ` SELECT
+        category,
+        COUNT(*) as total
+        FROM
+            project
+        GROUP BY category;`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ projectAccom: result })
+        })
+    },
+    latestProject : (req,res) => {
+        let sql = `select * from project order by date DESC limit 2`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ project: result })
+        })
+    },
+    project : (req,res) => {
+        let sql = `select * from project`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ project: result })
+        })
+    },
+    projectTitle : (req,res) => {
+        let sql = `select * from global where type = "project_title"`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ projectTitle: result })
+        })
+    },
+    slider : (req,res) => {
+        let sql = `select * from home_slider`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ slider: result })
+        })
+    },
+    studies : (req,res) => {
+        let sql = `select * from studies`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ studies: result })
+        })
+    },
+    testimonials : (req,res) => {
+        let sql = `select * from testimonials`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ testimonials: result })
+        })
+    },
+    our_team : (req,res) => {
+        let sql = `select * from our_team`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ our_team: result })
+        })
+    },
+    partnership : (req,res) => {
+        let sql = `select * from partnership`
+
+        db.query(sql,(err,result)=>{
+
+            res.json({ partnership: result })
         })
     },
 }
--- routes/web.js
+++ routes/web.js
@@ -4,5 +4,20 @@ const controller        = require('../controllers/web');
 
 router.use(bodyParser.json())
 
+router.get('/career', controller.career)
+router.get('/category', controller.category)
+router.get('/faq', controller.faq)
+router.get('/intouch', controller.intouch)
+router.get('/projectAccom', controller.projectAccom)
+router.get('/latestProject', controller.latestProject)
+router.get('/project', controller.project)
+router.get('/projectTitle', controller.projectTitle)
+router.get('/slider', controller.slider)
+router.get('/studies', controller.studies)
+router.get('/testimonials', controller.testimonials)
+router.get('/our_team', controller.our_team)
+router.get('/partnership', controller.partnership)
+
+
 
 module.exports = router;
